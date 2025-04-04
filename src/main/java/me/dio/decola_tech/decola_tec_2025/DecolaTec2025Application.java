d0c4e08c1c5f72fedbfed50fd9ef67b7bfd29091
Jônatas Talmon <jonatas_talmon@hotmail.com>
2025-04-04 18:49:33 -0300

    Initial commit

---
+++ src/main/java/me/dio/decola_tech/decola_tec_2025/DecolaTec2025Application.java
@@ -0,0 +1,13 @@
+package me.dio.decola_tech.decola_tec_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTec2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTec2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/decola_tech/decola_tec_2025/DecolaTec2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.decola_tech.decola_tec_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTec2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
